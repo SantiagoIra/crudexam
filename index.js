4147d68c0f24f6ebd3e45b382ece76b603e52df5
SantiagoIra <amiel.santiago3@gmail.com>
2020-02-13 15:43:04 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    port: 3306,
+    database: 'inventory'
+});
+
+mysqlConnection.connect((err)=>{
+    if(!err)
+    console.log('Connection Successful');
+    else
+    console.log('Connection failed' + JSON.stringify(err,undefined,2));
+});
+
+app.listen(3000,()=>console.log('Express server is running at port 3000'));
+
+app.get('/items',(res,req)=>{
+    mysqlConnection.query('select * from items', (err, rows, fields)=>{
+        if(!err)
+        console.log(rows);
+        else
+        console.log(err);
+    })
+}); 
