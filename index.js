50dc2e656cf9e013e18a1ecac6961bc4f1b0d7a8
SantiagoIra <amiel.santiago3@gmail.com>
2020-02-13 17:07:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var mysqlConnection = mysql.createConnection({
     password: 'password',
     port: 3306,
     database: 'inventory'
+    
+    
 });
 
 mysqlConnection.connect((err)=>{
@@ -22,6 +24,7 @@ mysqlConnection.connect((err)=>{
 
 app.listen(3000,()=>console.log('Express server is running at port 3000'));
 
+//select all
 app.get('/items',(req,res)=>{
     mysqlConnection.query('select * from items', (err, rows, fields)=>{
         if(!err)
@@ -30,3 +33,37 @@ app.get('/items',(req,res)=>{
         console.log(err);
     })
 }); 
+
+//get 
+app.get('/items/:id',(req,res)=>{
+    mysqlConnection.query('select * from items where id = ?',[req.params.id] ,(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+//delete
+app.delete('/items/:id',(req,res)=>{
+    mysqlConnection.query('delete from items where id = ?',[req.params.id] ,(err, rows, fields)=>{
+        if(!err)
+        res.send('Deleted Successfully');
+        else
+        console.log(err);
+    })
+});
+
+//insert
+app.post('/items',(req,res)=>{
+    let emp = req.body;
+    var sql = "set @id = ?; set @name = ?; set @qty = ?; set @amount = ?; \
+    call ItemsAddOrEdit(@id,@name,@qty,@amount);";
+    mysqlConnection.query(sql,[emp.id, emp.name, emp.qty, emp.amount],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
