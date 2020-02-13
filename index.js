c548135e342f6bba9d915e544e085463c0933ed8
SantiagoIra <amiel.santiago3@gmail.com>
2020-02-13 16:10:38 +0800

    Database connected

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ mysqlConnection.connect((err)=>{
 
 app.listen(3000,()=>console.log('Express server is running at port 3000'));
 
-app.get('/items',(res,req)=>{
+app.get('/items',(req,res)=>{
     mysqlConnection.query('select * from items', (err, rows, fields)=>{
         if(!err)
-        console.log(rows);
+        res.send(rows);
         else
         console.log(err);
     })
